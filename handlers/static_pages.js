76fbbb4c2ef415ff7bbe5178d2de30cad3835135
Damian Perez <cyberdante@gmail.com>
2017-02-06 18:09:28 -0500

    Adds team members data to the json and custom css

--- handlers/static_pages.js
+++ handlers/static_pages.js
@@ -3,10 +3,35 @@
 // DATA =======================================================================
 const team_members = [
   {
-    first_name: 'Ronald',
-    last_name: 'Rodriguez',
-    position: 'Jr Developer',
-    picture_url: 'img/avatar-team-1.jpg',
+    first_name: 'Rudy',
+    last_name: 'Ferraz',
+    position: 'Founder / CEO',
+    picture_url: 'img/team/Rudy.png',
+    social_links: {
+      twitter: '#',
+      dribble: '#',
+      vimeo: '#',
+      instagram: '#',
+      linkedin: 'https://www.linkedin.com/in/rudyferraz'
+    }
+  },
+  {
+    first_name: 'Frances',
+    last_name: 'Ferraz',
+    position: 'Founder / CFO',
+    picture_url: 'img/team/Frances.png',
+    social_links: {
+      twitter: '#',
+      dribble: '#',
+      vimeo: '#',
+      instagram: '#'
+    }
+  },
+  {
+    first_name: 'Matthew',
+    last_name: 'Stahlmann',
+    position: 'Founder / CTO',
+    picture_url: 'img/team/Matthew.png',
     social_links: {
       twitter: 'http://www.twitter.com/ronrsuarez',
       dribble: '#',
@@ -17,13 +42,85 @@ const team_members = [
   {
     first_name: 'Damian',
     last_name: 'Perez',
-    position: 'Sr Dev',
-    picture_url: 'img/avatar-team-1.jpg',
+    position: 'Senior Software Developer',
+    picture_url: 'img/team/Damian.jpg',
+    social_links: {
+      twitter: 'https://twitter.com/compscidamian',
+      vimeo: 'https://vimeo.com/cyberdante',
+      instagram: 'https://www.instagram.com/compscidamian/',
+      linkedin: 'https://www.linkedin.com/in/perezdamian'
+    }
+  },
+  {
+    first_name: 'Todd',
+    last_name: 'Bluhm',
+    position: 'Software Developer',
+    picture_url: 'img/team/Todd.jpg',
+    social_links: {
+      twitter: 'https://twitter.com/toddbluhm',
+      linkedin: 'https://www.linkedin.com/in/toddbluhm',
+      vimeo: '#',
+      instagram: '#'
+    }
+  },
+  {
+    first_name: 'Ronald',
+    last_name: 'Rodriguez',
+    position: 'Junior Software Developer',
+    picture_url: 'img/team/Ronald.jpg',
+    social_links: {
+      twitter: 'http://www.twitter.com/ronrsuarez',
+      dribble: '#',
+      vimeo: '#',
+      instagram: '#'
+    }
+  },
+  {
+    first_name: 'Chris',
+    last_name: 'Long',
+    position: 'Junior Marketing Associate',
+    picture_url: 'img/team/ChrisLong.jpg',
+    social_links: {
+      twitter: 'https://twitter.com/TheClassicLong',
+      linkedin: 'https://www.linkedin.com/in/christopher-long-0223126a',
+      vimeo: '#',
+      instagram: '#'
+    }
+  },
+  {
+    first_name: 'Danni',
+    last_name: 'Turner',
+    position: 'Junior Marketing Associate',
+    picture_url: 'img/team/Danni.jpg',
     social_links: {
       twitter: '#',
       dribble: '#',
       vimeo: '#',
       instagram: '#'
+    }
+  },
+  {
+    first_name: 'Benjamin',
+    last_name: 'Jerew',
+    position: 'Senior Technical Writer',
+    picture_url: 'img/team/Benjamin.jpg',
+    social_links: {
+      twitter: 'https://twitter.com/LoneWolffe',
+      linkedin: 'https://www.linkedin.com/in/jerewindependentresearch',
+      vimeo: '#',
+      instagram: '#'
+    }
+  },
+  {
+    first_name: 'Keri-Lynn',
+    last_name: 'Mitoff',
+    position: 'Senior UI/UX Designer',
+    picture_url: 'img/team/Keri-Lynn.jpg',
+    social_links: {
+      twitter: 'https://twitter.com/klm001',
+      dribble: 'https://dribbble.com/klm001',
+      linkedin: 'https://www.linkedin.com/in/keri-lynn-mitoff-2bb39a2',
+      instagram: 'https://www.instagram.com/klm_001'
     }
   }
 ];
